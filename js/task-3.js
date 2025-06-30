6dc4fac99779509179425bbd69e8d9feb96f4729
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-06-30 20:07:01 +0300

    add 3 tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,32 @@
 'use strict';
+
+class StringBuilder {
+  #value;
+
+  constructor(initialValue = '') {
+    if (typeof initialValue !== 'string') throw new Error('Not a string input');
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    this.#value += str;
+  }
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
+
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
